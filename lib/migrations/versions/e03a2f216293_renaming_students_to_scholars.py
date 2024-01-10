049f17c41639433520e32b37fdccc581a53566b0
Charlie Newlinds <cnewlinds@gmail.com>
2024-01-10 12:12:14 +0700

    manual migration

---
+++ lib/migrations/versions/e03a2f216293_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: e03a2f216293
+Revises: 791279dd0760
+Create Date: 2024-01-10 11:58:23.698415
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e03a2f216293'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
